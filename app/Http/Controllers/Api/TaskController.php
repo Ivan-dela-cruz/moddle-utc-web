cbaef771dd38ab13c6787e00eeaff5a101e07c00
Gilson Ariel <gilson258@live.com>
2021-02-12 01:16:03 -0500

    feat:(API TASKS)Api para la entrega de tareas

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\File;
 use App\Http\Controllers\Controller;
+use App\StudentTask;
 use App\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class TaskController extends Controller
 {
@@ -16,6 +21,7 @@ class TaskController extends Controller
 
         $list = new Collection();
         foreach ($tasks as $task) {
+
             $files = $task->files->count();
             $course_image = $task->Course->url_image;
             $item = [
@@ -28,6 +34,7 @@ class TaskController extends Controller
                 'end_time' => $task->end_time->format('H:m:i a'),
                 'status' => $task->status,
                 'course_id' => $task->course_id,
+                'deliveries' => $task->taskdeliveries->count(),
                 'files' => $files
             ];
             $list->push($item);
@@ -43,12 +50,51 @@ class TaskController extends Controller
     public function detailTask($task_id)
     {
         $task = Task::find($task_id);
+        return response()->json([
+            'success' => true,
+            'files' => $task->files,
+            'status' => 200
+        ], 200);
+    }
+
+    public function deliveryTask(Request $request)
+    {
+        $student = Auth::user()->student;
+        $size = $_FILES['files1']['size'];
+        $target1 = basename($_FILES['files1']['name']);
+        $filename = time() . ' - ' . $target1;
+        $filepath = public_path('tasks/delivery/');
+
+        $studentTask = StudentTask::create([
+            'description' => $request->description,
+            'course_id' => $request->course_id,
+            'student_id' => $student->id,
+            'task_id' => $request->task_id,
+        ]);
+
+        $fileUpload = new File([
+            'name' => $target1,
+            'filename' => $filename,
+            'size_file' => $size,
+            'url_file' => 'tasks/delivery/' . $filename
+        ]);
+        $student->files()->save($fileUpload);
+
+        $task = Task::find($request->task_id);
+        $task->update([
+           'status' => 'Finalizado'
+        ]);
+
+        move_uploaded_file($_FILES['files1']['tmp_name'], $filepath . $filename);
+
 
         return response()->json([
             'success' => true,
-            'files' =>  $task->files,
+            'files' => '',
             'status' => 200
         ], 200);
+
+
     }
 
 
